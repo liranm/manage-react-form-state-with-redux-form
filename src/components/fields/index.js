8b4212d2f60d56338bc4d1234f6e0f0eb74f78dd
Liran Mery <liranm@ironsrc.com>
2018-10-20 18:39:52 +0300

    Access the State of redux-form Fields using the  prop

--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ReactJson from 'react-json-view';
 
 export const customInput = props => (
     <div>
         <label>{props.label}</label>
         <input {...props.input} type={props.type} />
+        <ReactJson src={props.meta} />
     </div>
 );
 
@@ -14,5 +16,6 @@ export const customSelect = props => (
             <option value="tabs">Tabs</option>
             <option value="spaces">Spaces</option>
         </select>
+        <ReactJson src={props.meta} />
     </div>
 );
